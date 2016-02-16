80b893dd495fc16c3bbbdf38e3335b7ee26f4903
Adrian David Smith <sierra.as74@gmail.com>
2016-02-16 20:49:22 +0200

    Git Ignore | Gruntfile Stub | Package

---
+++ gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+  });
+
+  // Default task(s).
+  grunt.registerTask('default', function(){
+    grunt.log.ok("working!");
+  });
+
+};
