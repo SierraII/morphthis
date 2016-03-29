6b8618bd4dd329254c4862eedb3d43250bba63a5
Adrian David Smith <sierra.as74@gmail.com>
2016-03-29 13:56:36 +0200

    Sign Task

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,3 @@
-var chalk = require("chalk");
-
 /* -------------------------------------------------------------------- */
 /*
 
@@ -98,7 +96,7 @@ module.exports = function(grunt){
             }
         },
     },
-    
+
     open: {
         apk:{
             path: "./apk/build/"
@@ -113,44 +111,10 @@ module.exports = function(grunt){
   /* -------------------------------------------------------------------- */
   /*
 
-      Tasks
+      Load Tasks
 
   */
   /* -------------------------------------------------------------------- */
-
-  // core for signing the apk
-  grunt.registerTask("sign", function(){
-
-      // load the task timer
-      require("time-grunt")(grunt);
-
-      // PROCESS:
-      // copy the APK
-      // rename the file extention from APK to ZIP
-      // extract the contents
-      // delete the META-INF folder
-      // zip or compress the contents
-      // rename from ZIP to APK
-      // use the keytool to sign the APK
-      // zip align the newly signed APK
-
-      grunt.task.run("apk_info");
-      grunt.task.run("keystore_info");
-      grunt.task.run("apk_copy");
-      grunt.task.run("apk_rename_to_zip");
-      grunt.task.run("apk_unzip");
-      grunt.task.run("apk_delete_meta");
-      grunt.task.run("apk_compress");
-      grunt.task.run("apk_compress_rename");
-      grunt.task.run("apk_sign");
-      grunt.task.run("apk_signed_info");
-      grunt.task.run("apk_zip_align");
-      grunt.task.run("clean_build_contents");
-      grunt.task.run("show_done");
-      grunt.task.run("display_image");
-
-  });
-
   grunt.loadTasks("modules");
 
   /* -------------------------------------------------------------------- */
